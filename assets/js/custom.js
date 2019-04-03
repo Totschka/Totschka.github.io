f87413eb0716e13627e0e24c82cc3d86078d491b
Totschka <sharplight@naver.com>
2019-04-04 00:40:49 +0900

    ss

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,10 @@
 window.Zepto = Zepto, void 0 === window.$ && (window.$ = Zepto),
     function(t, e, n) {
         t("a#sideSlide").click(function() {
-            t("#sidebar,a#slide,#fade").addClass("slide"), t("#open").hide(), t("#search").hide(), t("#close").show()
+            t("#sidebar,a#sideSlide,#fade").addClass("slide"), t(".open").hide(), t("#search").hide(), t(".close").show()
         });
     }(Zepto, window);
+$(window).scroll(function() {
+    if($(this).scrollTop() >= 400) $el.addClass('show');
+    else $el.removeClass('show');
+})
