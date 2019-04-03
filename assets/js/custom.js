13c658df14e87c43e7c3343a191ea83090357b21
Totschka <sharplight@naver.com>
2019-04-04 01:15:45 +0900

    aa

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
-window.Zepto = Zepto, void 0 === window.$ && (window.$ = Zepto),
+/*window.Zepto = Zepto, void 0 === window.$ && (window.$ = Zepto),
     function(t, e, n) {
         t("a#sideSlide").click(function() {
             t("#sidebar,a#sideSlide,#fade").addClass("slide"), t(".open").hide(), t("#search").hide(), t(".close").show()
         });
-    }(Zepto, window);
+    }(Zepto, window);*/
 $(window).scroll(function() {
     var $btn = $("#sideSlide");
     if($(this).scrollTop() >= $(".header-post").height()) $btn.addClass('sidePop');
